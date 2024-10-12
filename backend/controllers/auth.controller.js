67e3f918f839cf9619f771f29bf13d69b745c5f9
Namit Malasi <namitmalasi@gmail.com>
2024-10-12 20:31:46 +0530

    database connection,created models, routes and controllers for auth

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,69 @@
+import User from "../models/user.model.js";
+export const register = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const user = new User({ name, email, password });
+    await user.save();
+
+    const token = jwt.sign({ email }, process.env.JWT_SECRET_KEY, {
+      expiresIn: "1h",
+    });
+    res.cookie("token", token, { httpOnly: true });
+
+    return res.status(201).json({
+      user: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+      message: "User registered successfully",
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Error in register controller", error: error.message });
+  }
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (user && user.comparePassword(password)) {
+      const token = jwt.sign({ email }, process.env.JWT_SECRET_KEY, {
+        expiresIn: "1h",
+      });
+      res.cookie("token", token, { httpOnly: true });
+
+      return res.status(200).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+      });
+    } else {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error in login controller", error: error.message });
+  }
+};
+
+export const logout = (req, res) => {
+  try {
+    res.clearCookie("token");
+    return res.json({ message: "Logged out successfully" });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Server error", error: error.message });
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,22 @@
 import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/auth.route.js";
+import chartRoutes from "./routes/chart.route.js";
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () => {
-  console.log("Server started at port 5000");
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/data", chartRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server running at Port: ${PORT}`);
+  connectDB();
 });
